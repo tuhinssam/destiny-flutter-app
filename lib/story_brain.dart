6ca49a9b7df71a4ac633584bc0cb63cdee229a52
tuhinssam <tuhinssam@gmail.com>
2021-06-20 15:37:41 +0530

    ToDo: completed all steps

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -48,46 +48,46 @@ class StoryBrain {
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData.first.choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData.first.choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
 //TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-  int storyNumber = 0;
+  int _storyNumber = 0;
 //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber
 // which will be the choice number (int) made by the user.
   void nextStory(int choiceNumber) {
-    if (storyNumber == 0) {
+    if (_storyNumber == 0) {
       if (choiceNumber == 1) {
-        storyNumber = 2;
+        _storyNumber = 2;
       } else {
-        storyNumber = 1;
+        _storyNumber = 1;
       }
-    } else if (storyNumber == 1) {
+    } else if (_storyNumber == 1) {
       if (choiceNumber == 1) {
-        storyNumber = 2;
+        _storyNumber = 2;
       } else {
-        storyNumber = 3;
+        _storyNumber = 3;
       }
-    } else if (storyNumber == 2) {
+    } else if (_storyNumber == 2) {
       if (choiceNumber == 1) {
-        storyNumber = 5;
+        _storyNumber = 5;
       } else {
-        storyNumber = 4;
+        _storyNumber = 4;
       }
-    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restart();
     }
   }
 
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
@@ -96,4 +96,11 @@ class StoryBrain {
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
